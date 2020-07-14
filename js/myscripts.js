b750205c74fd54551aba9b6aac5ee9f5ae160cf1
cschmity <68263550+cschmity@users.noreply.github.com>
2020-07-14 00:48:55 -0400

    Add files via upload

---
+++ js/myscripts.js
@@ -0,0 +1,20 @@
+/* Scroll to top */
+function scrollToTop() {
+    var position =
+        document.body.scrollTop || document.documentElement.scrollTop;
+    if (position) {
+        window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+        scrollAnimation = setTimeout("scrollToTop()", 20);
+    } else clearTimeout(scrollAnimation);
+}
+/* slide in */
+$(window).scroll(function() {
+  $(".slideanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("slide");
+    }
+  });
+});
