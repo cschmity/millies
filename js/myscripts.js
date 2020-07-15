9e39df095581140307dcc313fb6219a20cdabf63
cschmity <68263550+cschmity@users.noreply.github.com>
2020-07-14 23:59:08 -0400

    Add files via upload

--- js/myscripts.js
+++ js/myscripts.js
@@ -7,14 +7,3 @@ function scrollToTop() {
         scrollAnimation = setTimeout("scrollToTop()", 20);
     } else clearTimeout(scrollAnimation);
 }
-/* slide in */
-$(window).scroll(function() {
-  $(".slideanim").each(function(){
-    var pos = $(this).offset().top;
-
-    var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
-      $(this).addClass("slide");
-    }
-  });
-});
